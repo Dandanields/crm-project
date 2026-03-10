92dd77d22c441b87bae58009c61619007d195faf
Dandanields <121231606+Dandanields@users.noreply.github.com>
2026-03-10 12:17:19 +0100

    start maven and postman

--- crm-backend/src/main/java/com/daniel/crm_backend/CustomerController.java
+++ crm-backend/src/main/java/com/daniel/crm_backend/CustomerController.java
@@ -1,25 +1,14 @@
 package com.daniel.crm_backend;
 
-import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/customers")
-@CrossOrigin(origins = "*") // Erlaubt Zugriff vom Frontend-Port
 public class CustomerController {
-    private List<Customer> customers = new ArrayList<>();
 
-    @GetMapping
-    public List<Customer> getAllCustomers() {
-        return customers;
+    @GetMapping("/greet")
+    public String hello() {
+        return "Hello World";
     }
-
-    @PostMapping
-    public Customer addCustomer(@RequestBody Customer data) {
-        // Erstellt ein neues Objekt mit deiner ID-Logik (TKD-001)
-        Customer newCustomer = new Customer(data.getFirstname(), data.getLastname(), data.getEmail());
-        customers.add(newCustomer);
-        return newCustomer;
-    }
+    
 }
