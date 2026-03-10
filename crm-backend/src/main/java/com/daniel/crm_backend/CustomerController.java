540bd190eee8e20d8695163d0260a64202ab30fb
Dandanields <daniel.sok.2003@gmail.com>
2026-03-10 13:45:12 +0100

    Request parameter

--- crm-backend/src/main/java/com/daniel/crm_backend/CustomerController.java
+++ crm-backend/src/main/java/com/daniel/crm_backend/CustomerController.java
@@ -1,14 +1,15 @@
 package com.daniel.crm_backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 
     @GetMapping("/greet")
-    public String hello() {
-        return "Hello World";
+    public String hello(@RequestParam(value = "name") String name) {
+        return "Hello "+ name;
     }
     
 }
